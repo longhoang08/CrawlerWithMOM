bca69388cf2001b83ac478d45af6b7a57e310b6c
longhoang08 <longhoang0811@gmail.com>
2020-04-23 19:25:46 +0700

    kafka: create topic & get topic service

--- app/services/job_queue.py
+++ app/services/job_queue.py
@@ -14,7 +14,11 @@ def crawl_data_from_urls(urls: List[str]):
     return "DONE"
 
 
-def create_topics():
+def create_topics(topic_name: str):
     # example usage of kafka
     topic_list = []
+    topic_list.append(NewTopic(name=topic_name, num_partitions=1, replication_factor=1))
     admin_client.create_topics(new_topics=topic_list, validate_only=False)
+
+def get_topics(topic_name: str):
+    return admin_client.describe_topics(topic_name)
